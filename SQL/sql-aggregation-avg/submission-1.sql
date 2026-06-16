44f180476463200a96453ce595fedfd7cc5db3c8
Yerlen Saifulla <127534427+yerlenn@users.noreply.github.com>
2026-06-16 22:45:49 +0900

    Add: sql-aggregation-avg - submission-1

---
+++ SQL/sql-aggregation-avg/submission-1.sql
@@ -0,0 +1,23 @@
+CREATE TABLE scores (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    score INTEGER,
+    subject TEXT
+);
+
+INSERT INTO scores (score, subject) VALUES
+(72, 'math'),
+(88, 'math'),
+(68, 'math'),
+(80, 'science'),
+(90, 'science'),
+(60, 'science'),
+(90, 'history'),
+(85, 'history'),
+(100, 'history');
+-- Do not modify above this line. --
+
+SELECT ROUND(AVG(score)) AS average_math_score FROM scores
+WHERE subject = 'math';
+
+
+
