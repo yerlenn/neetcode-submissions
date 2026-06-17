4f6d88e792865475e13f346e35611ff6022fd374
Yerlen Saifulla <127534427+yerlenn@users.noreply.github.com>
2026-06-17 22:26:50 +0900

    Add: sql-subqueries-from - submission-3

---
+++ SQL/sql-subqueries-from/submission-3.sql
@@ -0,0 +1,33 @@
+CREATE TABLE employees (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name TEXT,
+    salary INTEGER,
+    department TEXT
+);
+
+INSERT INTO employees (name, salary, department) VALUES
+  ('Alice', 50000, 'marketing'),
+  ('Bob', 60000, 'marketing'),
+  ('Charlie', 55000, 'marketing'),
+  ('David', 65000, 'marketing'),
+  ('Eve', 70000, 'finance'),
+  ('Frank', 52000, 'finance'),
+  ('Grace', 58000, 'finance'),
+  ('Hank', 62000, 'finance');
+-- Do not modify above this line. --
+
+
+SELECT name, salary
+FROM employees
+JOIN (
+    SELECT AVG(salary) as avg_salary
+    FROM employees 
+    WHERE department = 'marketing'
+) AS avg
+ON employees.salary < avg.avg_salary
+WHERE department = 'marketing'
+ORDER BY salary;
+
+
+
+
